bdcf203dc92321306fb928d5f67893b95aa44894
K-Love <35044866+K-Love@users.noreply.github.com>
2021-06-29 06:43:19 -0700

    Fix broken before and after images

--- src/pages/before-and-after.js
+++ src/pages/before-and-after.js
@@ -8,16 +8,17 @@ import beforeafter3 from '../assets/images/heather-before-and-after.jpg';
 import beforeafter4 from '../assets/images/janice-before-and-after.jpg';
 import beforeafter5 from '../assets/images/karin-before-and-after.jpg';
 import beforeafter7 from '../assets/images/monique-before-after.jpg';
-import jessy from '..assets/images/before-and-after2.jpg';
-import beforeafter8 from '../assets/images/before-and-after8.jpg';
-import beforeafter9 from '../assets/images/before-and-after9.jpg';
-import beforeafter10 from '../assets/images/before-and-after10.jpg';
 import beforeafter11 from '../assets/images/before-and-after11.jpg';
 import beforeafter12 from '../assets/images/before-and-after12.jpg';
 import beforeafter14 from '../assets/images/before-and-after14.jpg';
 import beforeafter15 from '../assets/images/before-and-after15.jpg';
 import beforeafter16 from '../assets/images/before-and-after16.jpg';
 import beforeafter17 from '../assets/images/before-and-after17.jpg';
+import jessy from '../assets/images/before-and-after2.jpg';
+import beforeafter8 from '../assets/images/before-and-after8.png';
+import beforeafter9 from '../assets/images/before-and-after9.png';
+import beforeafter10 from '../assets/images/before-and-after10.png';
+
 
 const BeforePage = () => (
   <Layout fullMenu>
@@ -50,18 +51,6 @@ const BeforePage = () => (
               <img src={beforeafter7} className="img-shadow" alt="Fitness results for Monique" />
             </div>
             <div className="masonry-item">
-              <img src={jessy} className="img-shadow" alt="Fitness results for Jessy" />
-            </div>
-            <div className="masonry-item">
-              <img src={beforeafter8} className="img-shadow" alt="More fitness results from Chris Willburn Coaching" />
-            </div>
-            <div className="masonry-item">
-              <img src={beforeafter9} className="img-shadow" alt="Before and after results from working with Chris Willburn" />
-            </div>
-            <div className="masonry-item">
-              <img src={beforeafter10} className="img-shadow" alt="Chris Willburn gets you results" />
-            </div>
-            <div className="masonry-item">
               <img src={beforeafter11} className="img-shadow" alt="Before and after working with Chris Willburn" />
             </div>
             <div className="masonry-item">
@@ -78,6 +67,18 @@ const BeforePage = () => (
             </div>
             <div className="masonry-item">
               <img src={beforeafter17} className="img-shadow" alt="Lose weight with Chris Willburn Coaching" />
+            </div>
+            <div className="masonry-item">
+              <img src={jessy} className="img-shadow" alt="Fitness results for Jessy" />
+            </div>
+            <div className="masonry-item">
+              <img src={beforeafter8} className="img-shadow" alt="More fitness results from Chris Willburn Coaching" />
+            </div>
+            <div className="masonry-item">
+              <img src={beforeafter9} className="img-shadow" alt="Before and after results from working with Chris Willburn" />
+            </div>
+            <div className="masonry-item">
+              <img src={beforeafter10} className="img-shadow" alt="Chris Willburn gets you results" />
             </div>
           </div>
 
