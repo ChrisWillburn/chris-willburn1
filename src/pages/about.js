08318ba19837aa7a5385a6397f295b73ccf936de
K-Love <35044866+K-Love@users.noreply.github.com>
2021-07-12 08:18:40 -0700

    remove video thumbnail from about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 
-import aboutthumb from '../assets/images/aboutthumb.jpg';
+// import aboutthumb from '../assets/images/aboutthumb.jpg';
 
 const AboutPage = () => (
   <Layout fullMenu>
